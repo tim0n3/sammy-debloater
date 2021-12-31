49708f23b4050c0b911460228a4ca01988d77d95
tim0n3 <33823820+tim0n3@users.noreply.github.com>
2021-12-31 21:52:47 +0200

    Create general.bat

---
+++ general.bat
@@ -0,0 +1,68 @@
+:: RAW Paste of commands run on my samsung phone
+:: start adb server
+adb devices
+:: connect to adb shell
+adb shell
+:: remove samsung bloatware safely
+pm uninstall -k --user 0 com.samsung.android.bixby.agent
+pm uninstall -k --user 0 com.samsung.android.bixby.es.globalaction
+pm uninstall -k --user 0 com.samsung.android.bixby.wakeup
+pm uninstall -k --user 0 com.samsung.android.app.spage
+pm uninstall -k --user 0 com.samsung.android.visionintelligence
+pm uninstall -k --user 0 com.samsung.android.bixby.agent.dummy
+pm uninstall -k --user 0 samsung.android.app.settings.bixby
+pm uninstall -k --user 0 com.samsung.systemui.bixby2
+pm uninstall -k --user 0 com.samsung.android.bixby.service
+pm uninstall -k --user 0 com.samsung.android.bixby.plmsync
+pm uninstall -k --user 0 com.samsung.android.bixby.voiceinput
+pm uninstall -k --user 0 com.samsung.android.bixby.wakeup
+pm uninstall -k --user 0 com.samsung.android.app.settings.bixby
+pm uninstall -k --user 0 com.samsung.android.bixbyvision.framework
+pm uninstall -k --user 0 com.samsung.android.aremoji
+pm uninstall -k --user 0 com.samsung.android.arzone
+pm uninstall -k --user 0 flipboard.boxer.app
+pm uninstall -k --user 0 com.sec.android.mimage.avatarstickers
+pm uninstall -k --user 0 com.android.bips
+pm uninstall -k --user 0 com.samsung.android.email.provider
+pm uninstall -k --user 0 com.samsung.android.app.camera.sticker.facearavatar.preload
+pm uninstall -k --user 0 com.samsung.android.app.ledbackcover
+pm uninstall -k --user 0 com.sec.android.cover.ledcover
+pm uninstall -k --user 0 com.samsung.android.service.peoplestripe
+pm uninstall -k --user 0 com.microsoft.appmanager
+pm uninstall -k --user 0 com.microsoft.skydrive
+pm uninstall -k --user 0 com.microsoft.office.officehubrow
+pm uninstall -k --user 0 com.skype.raider
+pm uninstall -k --user 0 com.samsung.android.mateagent 
+pm uninstall -k --user 0 com.samsung.android.app.watchmanager
+pm uninstall -k --user 0 com.samsung.android.app.watchmanagerstub
+pm uninstall -k --user 0 com.samsung.android.voc
+pm uninstall -k --user 0 com.samsung.sree
+pm uninstall -k --user 0 com.samsung.android.stickercenter
+pm uninstall -k --user 0 com.samsung.android.livestickers
+pm uninstall -k --user 0 com.samsung.svoice.sync
+pm uninstall -k --user 0 com.samsung.android.svoiceime
+pm uninstall -k --user 0 com.samsung.android.svoice
+pm uninstall -k --user 0 com.sec.android.widgetapp.easymodecontactswidget
+pm uninstall -k --user 0 com.sec.android.widgetapp.webmanual
+pm uninstall -k --user 0 com.yelp.android.samsungedge
+pm uninstall -k --user 0 com.cnn.mobile.android.phone.edgepanel
+pm uninstall -k --user 0 com.sec.android.app.kidshome
+pm uninstall -k --user 0 com.samsung.android.kidsinstaller
+pm uninstall -k --user 0 com.samsung.android.fast
+pm uninstall -k --user 0 com.google.android.partnersetup
+pm uninstall -k --user 0 com.android.providers.partnerbookmarks
+pm uninstall -k --user 0 com.google.android.youtube
+pm uninstall -k --user 0 com.google.android.videos
+pm uninstall -k --user 0 com.google.android.music
+pm uninstall -k --user 0 com.google.android.apps.tachyon
+pm uninstall -k --user 0 flipboard.boxer.app
+pm uninstall -k --user 0 flipboard.app
+pm uninstall -k --user 0 com.dsi.ant.sample.acquirechannels
+pm uninstall -k --user 0 com.dsi.ant.service.socket
+pm uninstall -k --user 0 com.dsi.ant.server
+pm uninstall -k --user 0 com.dsi.ant.plugins.antplus
+pm uninstall -k --user 0 de.axelspringer.yana.zeropage
+pm uninstall -k --user 0 com.facebook.system
+pm uninstall -k --user 0 com.facebook.appmanager
+pm uninstall -k --user 0 com.facebook.services
+pm uninstall -k --user 0 com.linkedin.android
